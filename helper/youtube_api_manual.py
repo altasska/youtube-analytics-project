f3695e9c277453ea1c846f33827f839fae3aab0b
Zhanna Popova <zh.denisovna.97@gmail.com>
2023-06-29 15:37:58 +0500

    add a correcr youtube api key

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -1,5 +1,5 @@
-import json
 import os
+import json
 
 # необходимо установить через: pip install google-api-python-client
 from googleapiclient.discovery import build
@@ -8,7 +8,7 @@ import isodate
 
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.getenv('YT_API_KEY')
+api_key: str = os.getenv('AIzaSyDlaOVoU_iHyK62ab1bUVBCqDoNHA3BpyY')
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
